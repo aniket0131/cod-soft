ac41086e47b6b119b2dd4f0541a5bf6e9196cdf3
Rupen Bhimani <rupenbhimani109@gmail.com>
2024-06-12 00:54:23 +0530

    code changes

--- frontend/src/Services/apiconnector.js
+++ frontend/src/Services/apiconnector.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://jobportal-bed.vercel.app",
+  baseURL: "https://jobportal-bed.vercel.app/api",
   withCredentials: true,
 });
 
