a5a346b3d1c4c1a98dd5b5ff00ac86c1223ad633
Rupen Bhimani <rupenbhimani109@gmail.com>
2024-06-12 00:26:36 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = 4000;
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://jobportallinked.vercel.app/', // Allow requests from this origin
     credentials: true, // Allow cookies and other credentials
   };
 // Init Middleware
